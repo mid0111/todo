c182fd95b3fbc23f1933aa83d61f81a8789ea5ef
mid0111 <m.tajima0111@gmail.com>
2013-10-09 20:47:05 +0900

    UserModelにインデックス追加.

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  name: String,
-  email: String,
-  password: String
+  name: { type: String,index: true },
+  email: { type: String },
+  password: { type: String}
 });
 
 module.exports = mongoose.model('User', userSchema);
