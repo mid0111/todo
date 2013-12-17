5a7bd8e2bbe28ab6abfaba9ef3d894a88d02f238
mid0111 <m.tajima0111@gmail.com>
2013-12-17 21:16:28 +0900

    コミットみす.

--- client/public/javascripts/todos/collection.js
+++ client/public/javascripts/todos/collection.js
@@ -4,15 +4,6 @@ define(function(require) {
   var Backbone = require('backbone');
   var Todo = require('app/todos/model');
 
-  // Model
-  var Todo = Backbone.Model.extend({
-    defaults: {
-      title: ''
-    },
-
-    idAttribute: '_id'
-  });
-
   // Collection
   var TodoCollection = Backbone.Collection.extend({
     model: Todo,
